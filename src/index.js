af64f00ed560a38f2163103cac3ef769483b32ce
nadialang99 <nadia.lang@additive.eu>
2024-03-10 17:27:17 +0100

    Added detailed data on weather and date

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()]; 
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
+}
+
 function updateTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let speedElement = document.querySelector("#speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(); // Get the current date and time
+    let formattedDate = formatDate(date);
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    speedElement.innerHTML = `${response.data.wind.speed} km/h`;
+    timeElement.innerHTML = formattedDate;
 }
-
-function searchCity(city) {
-    let apiKey = "134f3c2049b8o8faf56bft351c709b8c";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(updateTemperature);
-}
-
-
-
 
 function handleSearchSubmit(event) {
     event.preventDefault();
@@ -22,7 +34,12 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+function searchCity(city) {
+    let apiKey = "134f3c2049b8o8faf56bft351c709b8c";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updateTemperature);
+}
 
 searchCity("Lisbon");
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
